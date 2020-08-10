05ea44e2545c1a3a825ffdcbd1a102f7e8d61c9a
Ivan <ivan.kazadaev@technorely.com>
2020-08-11 00:45:41 +0300

    Updates

---
+++ precache-manifest.0170182f15ec89e54d489f2b86ec4853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "982525db044d46b478aedf56a8cdd929",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5b786916a5cebf07ac7",
+    "url": "./static/css/main.622de071.chunk.css"
+  },
+  {
+    "revision": "596fe23de820dc829fbb",
+    "url": "./static/js/2.9fdcc8ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9fdcc8ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b786916a5cebf07ac7",
+    "url": "./static/js/main.747219ed.chunk.js"
+  },
+  {
+    "revision": "a0fe3bb841c2a605d533",
+    "url": "./static/js/runtime-main.bc16e0ed.js"
+  }
+]);
